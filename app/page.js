a4da1f98268874e966c2afa9154a277c89721090
Oluwarotimi Adewale <merlinwaysgaming@gmail.com>
2023-03-26 18:17:50 +0100

    fixed

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className='w-full min-h-screen bg-gray-400 flex items-center flex-col py-4'>
+    <div className='w-full min-h-screen bg-gray-300 flex items-center flex-col py-4'>
       <Login />
     </div>
   )
